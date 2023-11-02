21381822fa8d8ec933eed8288273b057e6b94fd8
SmiledPolecat34 <versayo03@gmail.com>
2023-11-02 14:22:21 +0100

    Dernier changement

--- src/Components/PokemonDetails.js
+++ src/Components/PokemonDetails.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 const PokemonDetails = ({ details, onAddToPokedex }) => {
-  const baseUrl = '/images/pokemon/'; // Assurez-vous que cela correspond au chemin de votre répertoire d'images.
+  const baseUrl = '/images/pokemon/';
 
   return (
     <div>
       <p>Nom: {details.name}</p>
       <p>Numéro: {details.id}</p>
       <p>Types: {details.types.map((type) => type.type.name).join(', ')}</p>
-      {/* Construisez l'URL de l'image en utilisant l'URL de base. */}
       <img src={baseUrl + details.sprites.front_default} alt={details.name} />
       <button onClick={() => onAddToPokedex(details)}>
         Ajouter au Pokédex
